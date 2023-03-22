9ed1540f834cac7c3dbe908aa9ce977e50eda7c1
Areebey <areebrafiq125@gmail.com>
2023-03-23 04:04:27 +0500

    show messages

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ function App() {
   return (
     <Box bg={"red.50"}>
       <Container h={"100vh"} bg={'white'}>
-        <VStack h={'full'} bg={"telegram.100"} padding={'4 '} >
+        <VStack h={'full'} bg={"telegram.100"} padding={'4 '} overflowY={'auto'} >
 
           <Button colorScheme='red' w={"full"} >LOgOut</Button>
           <VStack h={'full'} w={'full'}>
             <Messages text="Sample text" />
             <Messages user="me" text="Sample text" />
+            <Messages user="me" text="Sample text" />
+            <Messages user="me" text="Sample text" />
+            <Messages user="me" text="Sample text" />
+            <Messages text="Sample text" />
+            <Messages text="Sample text" />
             <Messages text="Sample text" />
          </VStack>
          <form style={{width:"100%"}}>
@@ -23,7 +28,7 @@ function App() {
         </VStack>
       </Container>
     </Box>
-    
+    //  so now 19 min of tutorial complete next is working on fire base 
   );
 }
 
--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -4,8 +4,13 @@ import {HStack, Avatar, Text} from "@chakra-ui/react"
 const Messages = ({text,uri,user="other"}) => {
   return (
     <HStack alignSelf={user==="me"?"flex-end":"flex-start"} borderRadius={"base"} bg="telegram.200" paddingY={2} paddingX={4}>
+      {
+        user==="other" && <Avatar src={uri}/>
+      } 
         <Text>{text}</Text>
-        <Avatar src={uri} />
+        {
+           user==="me" && <Avatar src={uri}/>
+        }
     </HStack>
    
   )
